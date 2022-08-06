7f39e9531a48b7f567a3fe574ca085690b6b60ee
truongtran2212 <truongtran221298@gmail.com>
2022-08-06 11:33:27 +0700

    test

--- backend-codegym/src/main/java/com/codegym/repository/IBrandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.Brand;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
-public interface IBrandRepository extends PagingAndSortingRepository<Brand, Integer> {
-}
